b8d53b37cbc67f1103b7b9c3c72e4f91dcdb6d97
Martin Dubé <mdube@gosecure.ca>
2017-02-02 16:48:30 -0500

    Switched HTTP lib from Requests to urllib for better support of TLS, including Client Cert. Auth.

--- gophish/api/api.py
+++ gophish/api/api.py
@@ -1,4 +1,6 @@
 import requests
+import urllib
+import json
 
 from gophish.models import Error
 
@@ -8,7 +10,7 @@ api.py
 Base API endpoint class that abstracts basic CRUD operations.
 '''
 
-class APIEndpoint(object):
+class APIEndpoint_requests(object):
     """
     Represents an API endpoint for Gophish, containing common patterns
     for CRUD operations.
@@ -98,3 +100,123 @@ class APIEndpoint(object):
             return Error.parse(response.json())
 
         return self._cls.parse(response.json())
+
+'''
+Overwrite of the class from api.py to support urllib instead of Requests
+
+Base API endpoint class that abstracts basic CRUD operations.
+'''
+
+class APIEndpoint_urllib(object):
+    """
+    Represents an API endpoint for Gophish, containing common patterns
+    for CRUD operations.
+    """
+    def __init__(self, api, endpoint=None, cls=None):
+        """ Creates an instance of the APIEndpoint class.
+
+        Args:
+            api - Gophish.client - The authenticated REST client
+            endpoint - str - The URL path to the resource endpoint
+            cls - gophish.models.Model - The Class to use when parsing results
+        """
+        self.api = api
+        self.endpoint = endpoint
+        self._cls = cls
+
+    def get(self, resource_id=None):
+        """ Gets the details for one or more resources by ID
+        
+        Args:
+            cls - gophish.models.Model - The resource class
+            resource_id - str - The endpoint (URL path) for the resource
+
+        Returns:
+            One or more instances of cls parsed from the returned JSON
+        """
+
+        endpoint = self.endpoint
+
+        if resource_id:
+            endpoint = '{}{}'.format(endpoint, resource_id)
+
+        try:
+            response = self.api.execute("GET", endpoint)
+            jresponse = json.loads(response.read().decode('UTF-8'))
+
+            if resource_id:
+                return self._cls.parse(jresponse)
+    
+            return [self._cls.parse(resource) for resource in jresponse]
+
+        except urllib.error.HTTPError as e:
+            return Error.parse(jresponse)
+
+    def post(self, resource):
+        """ Creates a new instance of the resource.
+
+        Args:
+            resource - gophish.models.Model - The resource instance
+
+        """
+
+        endpoint = self.endpoint
+        
+        try:
+            post_data = json.dumps(resource.as_dict()).encode('UTF-8').decode('ascii').encode('UTF-8')
+            headers = {'Content-Type': 'application/json'}
+            response = self.api.execute("POST", endpoint, data=post_data, headers=headers)
+            jresponse = json.loads(response.read().decode('UTF-8'))
+            return self._cls.parse(jresponse)
+
+        except urllib.error.HTTPError as e:
+            print(e.read())
+            if e.code == 400: 
+                jresponse = json.loads(e.read().decode('UTF-8'))
+                return Error.parse(jresponse)
+
+    def put(self, resource):
+        """ Edits an existing resource
+
+        Args:
+            resource - gophish.models.Model - The resource instance
+        """
+        
+        endpoint = self.endpoint
+
+        if resource.id:
+            endpoint = '{}{}'.format(endpoint, resource.id)
+
+        response = self.api.execute("PUT", endpoint, json=resource.as_json())
+
+        if not respose.ok:
+            return Error.parse(response.json())
+
+        return self._cls.parse(response.json())
+
+    def delete(self, resource_id):
+        """ Deletes an existing resource
+
+        Args:
+            resource_id - int - The resource ID to be deleted
+        """
+
+        endpoint = self.endpoint
+
+        endpoint = '{}{}'.format(endpoint, resource_id)
+
+        try:
+            headers = {'Content-Type': 'application/json'}
+            response = self.api.execute("DELETE", endpoint)
+            jresponse = json.loads(response.read().decode('UTF-8'))
+            return self._cls.parse(jresponse)
+
+        except urllib.error.HTTPError as e:
+            print(e.read())
+            if e.code == 400: 
+                jresponse = json.loads(e.read().decode('UTF-8'))
+                return Error.parse(jresponse)
+
+
+APIEndpoint = APIEndpoint_urllib
+#APIEndpoint = APIEndpoint_requests
--- gophish/client.py
+++ gophish/client.py
@@ -1,4 +1,7 @@
 import requests
+from urllib.request import build_opener, install_opener, HTTPSHandler, \
+                                    Request, urlopen
+
 
 from gophish.api import (
     campaigns,
@@ -16,13 +19,30 @@ class GophishClient(object):
         self.host = host
         self._client_kwargs = kwargs
     
-    def execute(self, method, path, **kwargs):
-        """ Executes a request to a given endpoint, returning the result """
+    def _execute_requests(self, method, path, kwargs):
+        """ Executes a request using the requests HTTP library """
 
         url = "{}{}".format(self.host, path)
         kwargs.update(self._client_kwargs)
         response = requests.request(method, url, params={"api_key": self.api_key}, **kwargs)
         return response
+
+    def _execute_urllib(self, method, path, kwargs):
+        """ Executes a request using the urllib HTTP library """
+
+        url = "{}{}?api_key={}".format(self.host, path, self.api_key)
+        kwargs.update(self._client_kwargs)
+        req = Request(url,**kwargs)
+        req.get_method = lambda: method
+        response = urlopen(req)
+        return response
+
+    def execute(self, method, path, **kwargs):
+        """ Executes a request to a given endpoint, returning the result 
+		Temporary mapping between 2 sub-functions for tests """
+
+        #return self._execute_requests(method, path, kwargs)
+        return self._execute_urllib(method, path, kwargs)
 
 class Gophish(object):
     def __init__(self, api_key, host=DEFAULT_URL, client=GophishClient, **kwargs):
